dc05362f33ef0f6da6e4b00c4c5f5ecf6ad93157
Luke Rollans <luke@rollans.sh>
2023-06-12 08:15:32 +0800

    add untested implementation

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,28 @@ import * as core from "@actions/core"
 
 async function run(): Promise<void> {
   try {
-    return await Promise.resolve()
+    // Retrieve required inputs
+    const location = core.getInput("location", { required: true })
+
+    // Perform some basic input validation
+    assert(
+      fs.existsSync(location),
+      `.tool-versions location specified in \`location\` input does not exist: ${location}`
+    )
+
+    // Read the entirety of `.tool-versions` in to memory and split it up by line
+    const toolVersions = fs.readFileSync(location, "utf-8").split(/\r?\n/)
+
+    // Loop over each entry and provide the environment variable and output
+    for (const entry of toolVersions) {
+      const [tool, version] = entry.split(" ")
+      core.setOutput(`${tool}_version`, version)
+      core.exportVariable(`${tool.toUpperCase()}_VERSION`, version)
+    }
+
+    // Summarise for log output
+    core.notice(`${toolVersions.length} outputs provided`)
+    core.notice(`${toolVersions.length} environment variables exported`)
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
